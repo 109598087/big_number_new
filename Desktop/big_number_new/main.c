1ffa2339f3443b250d7a9b599db6294b3f868b6c
puppy <puppylionmonkey33@gmail.com>
2020-09-09 00:07:47 +0800

    ok -gcov

--- Desktop/big_number_new/main.c
+++ Desktop/big_number_new/main.c
@@ -2,25 +2,7 @@
 #include <stdlib.h>
 #define LEN 100
 
-//void test_oper(char input1[], char input2[], char op)
-//{
-//    char c[LEN];
-//    if(op == '+')
-//    {
-//        Add(input1, input2, c);
-//        re_Print(c);
-//    }
-//    if(op == '-')
-//    {
-//        Sub(input1, input2, c);
-//        re_Print(c);
-//    }
-//    if(op == '*')
-//    {
-//        Mul(input1, input2, c);
-//        re_Print(c);
-//    }
-//}
+
 void Add(char input1[], char input2[], int result[])
 {
     int sign_num = sign(input1, input2);
@@ -44,7 +26,7 @@ void Add(char input1[], char input2[], int result[])
         }
         for(i=0; i<LEN-1; i++)
         {
-            if(result[i]>10)
+            if(result[i]>=10)
             {
                 result[i+1] += result[i]/10;
                 result[i] = result[i]%10;
@@ -216,6 +198,26 @@ void Mul(char input1[], char input2[], int result[])
         if(input2[len2-i-1]=='-') continue;
         input2_int[i] = input2[len2-i-1]-'0';
     }
+    for(i=0; i<LEN; i++)
+    {
+        for(j=0; j<LEN; j++)
+        {
+            result[i+j]+=input1_int[j]*input2_int[i];
+            if(result[i+j]>=10)
+            {
+                result[i+j+1]+=result[i+j]/10;
+                result[i+j]=result[i+j]%10;
+            }
+        }
+    }
+    if(sign_num==1 || sign_num==2)
+    {
+        for(i=LEN-1; i>0; i--)
+        {
+            if(result[i]!=0) break;
+        }
+        result[i+1] = 100; //在print的時候看到100->'-'
+    }
 
 
 
@@ -265,18 +267,55 @@ int sign(char input1[], char input2[]) //0:++,1:+-, 2:-+, 3:--
     if(sign1_num == 1 && sign2_num == 0)return 2; //-+
     if(sign1_num == 1 && sign2_num == 1)return 3; //--
 }
-
-
-
-
-
+void test_oper(char input1[], char input2[], char op)
+{
+    int c[LEN*2] = {0};
+    switch(op)
+    {
+    case '+':
+        Add(input1, input2, c);
+        re_Print(c);
+        break;
+    case '-':
+        Sub(input1, input2, c);
+        re_Print(c);
+        break;
+    case '*':
+        Mul(input1, input2, c);
+        re_Print(c);
+        break;
+    }
+}
 
 int main()
 {
-    int temp[LEN] = {0};
-    //Add("-999", "11111", temp);
-    //Sub("111", "99", temp);
-    Mul("123456", "654", temp);
-    re_Print(temp);
+    test_oper("11111", "999", '+');
+    test_oper("11111", "-999", '+');
+    test_oper("-11111", "999", '+');
+    test_oper("-11111", "-999", '+');
+    test_oper("999", "11111", '+');
+    test_oper("999", "-11111", '+');
+    test_oper("-999", "11111", '+');
+    test_oper("-999", "-11111", '+');
+
+    test_oper("11111", "999", '-');
+    test_oper("11111", "-999", '-');
+    test_oper("-11111", "999", '-');
+    test_oper("-11111", "-999", '-');
+    test_oper("999", "11111", '-');
+    test_oper("999", "-11111", '-');
+    test_oper("-999", "11111", '-');
+    test_oper("-999", "-11111", '-');
+
+    test_oper("11111", "999", '*');
+    test_oper("11111", "-999", '*');
+    test_oper("-11111", "999", '*');
+    test_oper("-11111", "-999", '*');
+    test_oper("999", "11111", '*');
+    test_oper("999", "-11111", '*');
+    test_oper("-999", "11111", '*');
+    test_oper("-999", "-11111", '*');
+
+
     return 0;
 }
