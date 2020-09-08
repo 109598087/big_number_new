18255a1f1006f0eca966786dd8e2a179f373f4ae
puppy <puppylionmonkey33@gmail.com>
2020-09-08 23:34:07 +0800

    add sub ok

--- Desktop/big_number_new/main.c
+++ Desktop/big_number_new/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LEN 100
+
 //void test_oper(char input1[], char input2[], char op)
 //{
 //    char c[LEN];
@@ -60,7 +61,52 @@ void Add(char input1[], char input2[], int result[])
     }
     else //異號
     {
-        //改變input2的符號(加一個負號)
+        if(len1>len2)
+        {
+            for(i=0; i<LEN; i++)
+            {
+                result[i] = input1_int[i] - input2_int[i];
+            }
+            for(i=0; i<LEN-1; i++)
+            {
+                if(result[i] < 0)
+                {
+                    result[i+1] -= 1;
+                    result[i] = 10+result[i];
+                }
+            }
+            if(sign_num==2)
+            {
+                for(i=LEN-1; i>0; i--)
+                {
+                    if(result[i]!=0) break;
+                }
+                result[i+1] = 100; //在print的時候看到100->'-'
+            }
+        }
+        else //len2>len1
+        {
+            for(i=0; i<LEN; i++)
+            {
+                result[i] = input2_int[i] - input1_int[i];
+            }
+            for(i=0; i<LEN-1; i++)
+            {
+                if(result[i] < 0)
+                {
+                    result[i+1] -= 1;
+                    result[i] = 10+result[i];
+                }
+            }
+            if(sign_num==1)
+            {
+                for(i=LEN-1; i>0; i--)
+                {
+                    if(result[i]!=0) break;
+                }
+                result[i+1] = 100; //在print的時候看到100->'-'
+            }
+        }
 
     }
 
@@ -80,18 +126,98 @@ void Sub(char input1[], char input2[], int result[])
         if(input2[len2-i-1]=='-') continue;
         input2_int[i] = input2[len2-i-1]-'0';
     }
+
     if(sign_num==0 || sign_num==3)//同號
     {
-
+        //計算數字
+        if(len1>len2) //if左邊比右邊大
+        {
+            for(i=0; i<LEN; i++)
+            {
+                result[i] = input1_int[i] - input2_int[i];
+            }
+            for(i=0; i<LEN-1; i++)
+            {
+                if(result[i] < 0)
+                {
+                    result[i+1] -= 1;
+                    result[i] = 10+result[i];
+                }
+            }
+            if(sign_num==3)
+            {
+                for(i=LEN-1; i>0; i--)
+                {
+                    if(result[i]!=0) break;
+                }
+                result[i+1] = 100; //在print的時候看到100->'-'
+            }
+        }
+        else // len2>len1
+        {
+            for(i=0; i<LEN; i++)
+            {
+                result[i] = input2_int[i] - input1_int[i];
+            }
+            for(i=0; i<LEN-1; i++)
+            {
+                if(result[i] < 0)
+                {
+                    result[i+1] -= 1;
+                    result[i] = 10+result[i];
+                }
+            }
+            if(sign_num==0)
+            {
+                for(i=LEN-1; i>0; i--)
+                {
+                    if(result[i]!=0) break;
+                }
+                result[i+1] = 100; //在print的時候看到100->'-'
+            }
+        }
     }
     else //異號
     {
-        //改變input2的符號(加一個負號)
-//        Add();
+        for(i=0; i<LEN; i++)
+        {
+            result[i] = input1_int[i] + input2_int[i];
+        }
+        for(i=0; i<LEN-1; i++)
+        {
+            if(result[i]>=10)
+            {
+                result[i+1] += result[i]/10;
+                result[i] = result[i]%10;
+            }
+        }
+        if(sign_num==2)
+        {
+            for(i=LEN-1; i>0; i--)
+            {
+                if(result[i]!=0) break;
+            }
+            result[i+1] = 100; //在print的時候看到100->'-'
+        }
     }
 }
 void Mul(char input1[], char input2[], int result[])
 {
+    int sign_num = sign(input1, input2);
+    int i,j, input1_int[LEN]= {0}, input2_int[LEN]= {0}; // save no - int[]
+    int len1=strlen(input1), len2=strlen(input2);
+    for(i=0; i<len1; i++)
+    {
+        if(input1[len1-i-1]=='-') continue;
+        input1_int[i] = input1[len1-i-1]-'0';
+    }
+    for(i=0; i<len2; i++)
+    {
+        if(input2[len2-i-1]=='-') continue;
+        input2_int[i] = input2[len2-i-1]-'0';
+    }
+
+
 
 }
 void re_Print(int result[]) // ok
@@ -148,8 +274,9 @@ int sign(char input1[], char input2[]) //0:++,1:+-, 2:-+, 3:--
 int main()
 {
     int temp[LEN] = {0};
- //   Add("-9999", "-9876", temp);
-    Sub("99999", "-9876", temp);
+    //Add("-999", "11111", temp);
+    //Sub("111", "99", temp);
+    Mul("123456", "654", temp);
     re_Print(temp);
     return 0;
 }
